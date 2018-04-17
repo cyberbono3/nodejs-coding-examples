0c4eeb68974ce3acca49fc3fbc87fe2cd606eeb1
EC2 Default User <ec2-user@ip-172-31-26-147.ec2.internal>
2018-04-17 17:35:18 +0000

    Setting up MySQL database with Node.js

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+
+// constructions SQL using Javascript
+
+// knex migrate:make create_user_table
+
+
+// Created Migration: /home/ec2-user/environment/mysql/migrations/20180417161748_create_user_table.js
+
+///fill in up and down method in migrations
+
+
+// run migration  knex migrate:latest
+
+// knex migrate:make encrypt_user_password
+//Created Migration: /home/ec2-user/environment/mysql/migrations/20180417165034_encrypt_user_password.js
+
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    user: 'root',
+    password: 'password',
+    database: 'tutorial_node_database'
+  }
+}
